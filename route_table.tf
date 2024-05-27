eea66e1f6fece2315e2d1bc90781e364d2ec0605
murtuza-autonomix <141496096+murtuza-autonomix@users.noreply.github.com>
2024-05-27 20:33:16 +0530

    Update route_table.tf

--- route_table.tf
+++ route_table.tf
@@ -9,8 +9,10 @@ resource "aws_route_table" "rt" {
 
 }
 
+/*
 resource "aws_route_table_association" "a" {
 for_each = toset([for subnet in var.subnet_id: subnet] )
   subnet_id      = each.value
   route_table_id = aws_route_table.rt.id
 }
+*/
